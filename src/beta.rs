b61d76d5e854e5a80539c059d81ea8140879480d
jmptable <jmptable@mit.edu>
2013-10-09 20:58:27 -0400

    Processor can be reset.

--- src/beta.rs
+++ src/beta.rs
@@ -1,10 +1,34 @@
+fn each(v: &mut [u32], op: &fn(v: &mut u32)) {
+	let mut n = 0;
+	while n < v.len() {
+		op(&mut v[n]);
+		n += 1;
+	}
+}
+
 struct Beta {
 	pc:			u32,
-	register:	~[u32],
-	memory:		~[u8]
+	register:	[u32, ..31],
+	memory:		[u8, ..4096]
+}
+
+impl Beta {
+	fn reset(&mut self) {
+		self.pc = 0;
+		do each(self.register) |r| { *r = 0; }
+
+		//println(fmt!("reset. pc at %d", self.pc as int));
+	}
+
+	fn tick(&self) {
+		//println(fmt!("tick. pc at %d", self.pc as int));
+
+	}
 }
 
 fn main() {
-	let b = Beta { pc: 0u32, register: ~[0u32, ..31], memory: ~[0u8, ..4*1024] };
+	let mut b = ~Beta { pc: 10u32, register: [3u32, ..31], memory: [0u8, ..4*1024] };
+	b.reset();
+	b.tick();
 	println("success");
 }
