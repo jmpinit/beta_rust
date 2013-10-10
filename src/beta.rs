7697c3ec1c20f5b0d53c13378c925868b3f0daed
jmptable <jmptable@mit.edu>
2013-10-09 21:06:21 -0400

    Memory as a separate entity.

--- src/beta.rs
+++ src/beta.rs
@@ -6,10 +6,24 @@ fn each(v: &mut [u32], op: &fn(v: &mut u32)) {
 	}
 }
 
+struct Mem {
+	data:	[u8, ..4096]
+}
+
+impl Mem {
+	fn read(&self, addr: u32) -> u8 {
+		self.data[addr]
+	}
+
+	fn write(&mut self, addr: u32, val: u8) {
+		self.data[addr] = val;
+	}
+}
+
 struct Beta {
 	pc:			u32,
 	register:	[u32, ..31],
-	memory:		[u8, ..4096]
+	memory:		Mem
 }
 
 impl Beta {
@@ -27,7 +41,14 @@ impl Beta {
 }
 
 fn main() {
-	let mut b = ~Beta { pc: 10u32, register: [3u32, ..31], memory: [0u8, ..4*1024] };
+	let mut b = ~Beta {
+		pc: 10u32,
+		register: [3u32, ..31],
+		memory: Mem {
+			data: [0u8, ..4*1024]
+		}
+	};
+
 	b.reset();
 	b.tick();
 	println("success");
