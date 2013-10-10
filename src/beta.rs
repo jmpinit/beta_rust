1a4a693e6b993ddc5e933c2881a74d65ea7da9a3
jmptable <jmptable@mit.edu>
2013-10-09 21:14:53 -0400

    Memory can be read byte-wise or word-wise.

--- src/beta.rs
+++ src/beta.rs
@@ -15,6 +15,15 @@ impl Mem {
 		self.data[addr]
 	}
 
+	fn read_word(&self, addr: u32) -> u32 {
+		let b1 = self.read(addr) as u32;
+		let b2 = self.read(addr + 1) as u32;
+		let b3 = self.read(addr + 2) as u32;
+		let b4 = self.read(addr + 3) as u32;
+
+		(b1 << 24) | (b2 << 16) | (b3 << 8) | b4
+	}
+
 	fn write(&mut self, addr: u32, val: u8) {
 		self.data[addr] = val;
 	}
@@ -23,7 +32,7 @@ impl Mem {
 struct Beta {
 	pc:			u32,
 	register:	[u32, ..31],
-	memory:		Mem
+	mem:		Mem
 }
 
 impl Beta {
@@ -35,6 +44,8 @@ impl Beta {
 	}
 
 	fn tick(&mut self) {
+		let instruction: u32 = self.mem.read_word(self.pc);
+
 		self.pc += 4;
 		//println(fmt!("tick. pc at %d", self.pc as int));
 	}
@@ -44,7 +55,7 @@ fn main() {
 	let mut b = ~Beta {
 		pc: 10u32,
 		register: [3u32, ..31],
-		memory: Mem {
+		mem: Mem {
 			data: [0u8, ..4*1024]
 		}
 	};
